cbe77ba0de6cf9a694f792fcf8172fb17237e0be
jackieebenitez <jackieebenitez@gmail.com>
2016-12-06 18:27:33 +0000

    Etapa 4

--- js/script-index.js
+++ js/script-index.js
@@ -6,7 +6,11 @@ $(document).ready( function(){
 	renderHighlightedRecipes(recipesArray);
 
 });
- 
+
+function printNews () {
+	$(".callout-news p").append("Nuevas Recetas");
+}
+
 /*
 * Función que se encarga de pintar TODAS las recetas que tengan 
 * marcado el atributo "highlighted" como TRUE
@@ -19,8 +23,6 @@ function renderHighlightedRecipes(recipesArray) {
 			}
 	}
 }
-
-
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
 * marcado el atributo "highlighted" como TRUE
@@ -29,12 +31,43 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
-}
+	//<a class="item-recipe" href="#">
+	var a = $("<a></a>");
+	a.addClass('item-recipe');
+	//<span class="attribution">
+	var attribution = $("<span></span>");
+	attribution.addClass("attribution");
+	//<span class="title-recipe">
+	var titlerecipe = $("<span></span>");
+	titlerecipe.addClass("title-recipe");
+	titlerecipe.text(recipe.title);
+	//<span class="metadata-recipe">
+	var metadatarecipe = $("<span></span>");
+	metadatarecipe.addClass("metadata-recipe");
+	//<span class="author-recipe">
+	var authorrecipe = $("<span></span>");
+	authorrecipe.addClass("author-recipe");
+	authorrecipe.text(recipe.source.name);
+	// <span class="bookmarks-recipe">
+	var bookmarks = $("<span></span>");
+	bookmarks.addClass("bookmarks-recipe");
+	// <span class="icon-bookmark"></span>
+	var iconbookmark = $("<span></span>");
+	iconbookmark.addClass("icon-bookmark");
+	// img src
+	var img = $("<img/>");
+	img.attr("src", "img/recipes/320x350/" + recipe.name + ".jpg");
 
-function printNews () {
-	$(".callout-news p").append("Nuevas Recetas");
-}
+	bookmarks.append(iconbookmark);
+	metadatarecipe.append(bookmarks);
+	metadatarecipe.append(authorrecipe);
+	attribution.append(metadatarecipe);
+	attribution.append(titlerecipe);
+	a.append(img);
+	a.append(attribution);
 
+	$(".list-recipes").append(a);
+}
 
 
 /*
