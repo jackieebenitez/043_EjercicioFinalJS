acc1eb5adfffc3b58ff244b2916a3069509f6a22
jackieebenitez <jackieebenitez@gmail.com>
2016-12-07 15:41:30 +0000

    Etapa 8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,17 @@
 $(document).ready( function(){
-	console.log("READY!");
 	$(".js-menu").hide();
+	console.log("READY!");
+
+	$(".js-show-make").click(function(){
+    $(".page").addClass("make");
+    $(".js-show-make").addClass("active");
+   	$(".js-show-make").removeClass("active");
+	});
+
+	$(".js-show-recipe").click(function(){
+	$(".page").removeClass("make");
+	$(".js-show-recipe").removeClass("active");
+	$(".js-show-recipe").addClass("active");
+	});
+
 });
