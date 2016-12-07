dde48490644d124661fddffac75566e1303e6e8f
jackieebenitez <jackieebenitez@gmail.com>
2016-12-07 16:13:29 +0000

    Etapa 10

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
 	$(".js-menu").hide();
 	console.log("READY!");
-
+	//Etapa 8 y 9 
 	$(".js-show-make").click(function(){
 	    $(".page").addClass("make");
 	    $(".js-show-make").addClass("active");
@@ -12,6 +12,10 @@ $(document).ready( function(){
 		$(".page").removeClass("make");
 		$(".js-show-recipe").addClass("active");
 		$(".js-show-make").removeClass("active");
+	});
+	//Etapa 10
+	$(".js-back").click(function(){
+		window.location.href = "index.html";
 	});
 
 });
